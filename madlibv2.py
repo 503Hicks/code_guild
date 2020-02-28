78d05f24818cd3584e023969786779c2a2b10671
Monique Williamson <monique@kate-laptop.localdomain>
2020-02-27 17:40:02 -0800

    first save

---
+++ madlibv2.py
@@ -0,0 +1,22 @@
+
+
+# for i in range(3):
+#     nouns = input('enter three nouns')
+
+# nounlist = []
+# nounlist.append(nouns)
+
+
+
+
+
+
+user_input = input('enter a plural noun, an adjective, a verb, a noun, and anouther adjectieve')
+
+user_input_list = user_input(', ')
+
+print(user_input_list)
+
+output = f'it\s all the same, only the names will change everyday, it seems were wastin away another place where the {user_input_list[0]} are so {user_input_list[1]} I {user_input_list[2]} all night just to get back home im a {user_input_list[3]}, on a {user_input_list[4]} and something {user_input_list[5]}.
+
+print(output)
---
+++ practice.py
@@ -0,0 +1,39 @@
+nouns = input("give three nounds seperated by a space")
+nounds_list = nouns.split(', ')
+
+adj1 = input("Enter an adjective: ")
+adj2 = input("Enter another adjective: ")
+bird = input("Enter a type of bird: ")
+room = input("Enter a room in a house: ")
+verb1 = input("Enter a past tense verb: ")
+verb2 = input("Enter a verb: ")
+verb3 = input("Enter a verb inding in -ing: ")
+verb4 = input("Enter a verb ending in -ing: ")
+name = input("enter a relative's name: ")
+liquid = input("Enter a liquid: ")
+bodypart = input("Enter a part of the body; plural: ")
+# noun1 = input("Enter a noun: ")
+# noun2 = input("Enter a plural noun: ")
+# noun3 = input("Enter a noun: ")
+
+
+# nounlist = []
+# nounlist.append(nouns)
+
+
+# for i in range(3):
+#     nouns = input('enter three nouns')
+
+
+
+
+print(f'It was a {adj1}, cold November day. I woke up to the {adj2} smell of {bird} roasting in the {room} downstairs. I {verb1} down the stairs to see if I could help {verb2} the dinner. My mom said, "See if {name} needs a fresh {nouns[0]}." So I carried a tray of glasses full of {liquid} into the {verb3} room. When I got there, I couldn\'t believe my {bodypart}! There were {nouns[1]} {verb4} on the {nouns[2]}')
+
+
+
+
+user_input = input('do you want to continue?: ')
+
+while (user_input == 'yes'):
+    print('continuing...')
+    user_input = input('do you want to contiue: ')
