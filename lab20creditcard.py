3dd11f6c05497ce5b55a71fe8f9903f74bcb64b9
Monique Williamson <monique@Moniques-MacBook-Pro.local>
2020-03-24 20:44:45 -0700

    convtd input to list of ints,sliced last dig of cc

--- lab20creditcard.py
+++ lab20creditcard.py
@@ -1,24 +1,35 @@
+# Let's write a function which returns whether a string containing a credit card number is valid as a boolean. The steps are as follows:
+
+# Convert the input string into a list of ints
+# Slice off the last digit. That is the check digit.
+# Reverse the digits.
+# Double every other element in the reversed list.
+# Subtract nine from numbers over nine.
+# Sum all values.
+# Take the second digit of that sum.
+# If that matches the check digit, the whole card number is valid.
+# For example, the worked out steps would be:
+
+# 4 5 5 6 7 3 7 5 8 6 8 9 9 8 5 5
+# 4 5 5 6 7 3 7 5 8 6 8 9 9 8 5
+# 5 8 9 9 8 6 8 5 7 3 7 6 5 5 4
+# 10 8 18 9 16 6 16 5 14 3 14 6 10 5 8
+# 1 8 9 9 7 6 7 5 5 3 5 6 1 5 8
+# 85
+# 5
+# Valid!
 
 
+# Convert the input string into a list of ints
+cc_list = list(input('enter cc number: '))
+# Slice off the last digit. That is the check digit.
+cc_list = cc_list[:-1]
 
 
-cc_list
-cc_input = int(input('Enter CC number: '))
-
-print(cc_num)
-
+# >>> list('hello')
+# ['h', 'e', 'l', 'l', 'o']
+# Even without converting th
 
 
-user_list = []
+print(cc_list)
 
-while True:
-    #USERS INPUT OF NUMBERS TO BE AVERAGED
-    user_input = input('Enter a number or enter "done" when finished. ').lower()
-    if user_input == 'done':
-        break
-    user_list.append(int(user_input))
-#USERS AVERAGE
-list_average = sum(user_list)/len(user_list)
-#OUTPUT OF USERS LIST & AVERAGE OF SUM IN LIST
-print(user_list)
-print(f'Your average is {list_average}.')
