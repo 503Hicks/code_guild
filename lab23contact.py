a2fc63045a3c80cea781587dfe64a58d526473ff
Monique Williamson <monique@Moniques-MacBook-Pro.local>
2020-03-27 20:55:59 -0700

    made with open, split lines

--- lab23contact.py
+++ lab23contact.py
@@ -1,3 +1,14 @@
+# f = open('filename.txt')  # open the file
+# contents = f.read()  # read the contents
+# print(contents)
+# f.close()  # close the file
+import csv 
+
+
 with open('contacts.csv', 'r') as file:
     lines = file.read().split('\n')
     print(lines)
+
+# contacts= [{'name':'matthew', 'favorite fruit': 'blackberries', 'favorite color':'orange',},
+# {'name':'deaudre', 'favorite fruit': 'bananas', 'favorite color': 'green'}
+
