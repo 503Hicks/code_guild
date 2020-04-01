cf194bc9915e5ab0c4d793819a4b5e2a26239be0
Monique Williamson <monique@Moniques-MacBook-Pro.local>
2020-03-31 19:43:50 -0700

    def method for get_trasactions. appended deposits.

--- lab25atm.py
+++ lab25atm.py
@@ -1,45 +1,34 @@
-#lab 25 ATM
-
-# # 
-# Let's represent an ATM with a class containing two attributes: a balance and an interest rate. A newly created account will default to a balance of 0 and an interest rate of 0.1%. Implement the initializer, as well as the following functions:
-
-
-
 class ATM():
     def __init__(self, balance=0, print_transactions=[]):
         self.balance = balance
         self.print_transactions = print_transactions
     
-
-        
     def check_balance(self): # returns the account ballance
-        return f' your balance is {self.balance}'
+        return f'****Your balance is {self.balance}****'
 
-    def deposit(self, amount):
+    def deposit(self, amount): 
         self.balance = self.balance + amount
+        self.print_transcations = self.print_transactions.append(f'You deposited {amount}')
         return f'You deposited {amount} dollars. your new amount is {self.balance}'
+    
 
-    def check_withdrawal(self, amount): # check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the
+    def check_withdrawal(self, amount):#returns true if the withdrawn amount won't put the account in the neg.
         if amount < self.balance:
             return True
 
     def withdraw(self, amount):
         self.balance -= amount
         return f'You withdrew {amount} dollars. your new amount is {self.balance}'
-   
-   
-# check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the negative
-# withdraw(amount) withdraws the amount from the account and returns it
-# calc_interest() returns the amount of interest calculated on the account
-
-
+    
+    def get_trasactions(self):
+        return self.print_transactions
 
 my_atm1 = ATM(3000,[])
 
 print(my_atm1.check_balance())
-print(my_atm1.deposit(4582))
-print(my_atm1.check_balance())
-print(my_atm1.check_withdrawal(20))
-print(my_atm1.withdraw(100))
-print(my_atm1.withdraw(100))
-print(my_atm1.withdraw(100))
+print(my_atm1.deposit(500))
+print(my_atm1.deposit(1000))
+print(my_atm1.deposit(25))
+# print(my_atm1.print_transactions)
+
+print(my_atm1.get_trasactions())
