a3d5fdea8476dddf1e674946b50172c975e6a7fc
Monique Williamson <monique@Moniques-MacBook-Pro.local>
2020-03-31 18:24:21 -0700

    def deposit, def check_withdrawal, def withdraw

--- lab25atm.py
+++ lab25atm.py
@@ -9,18 +9,37 @@ class ATM():
     def __init__(self, balance=0, print_transactions=[]):
         self.balance = balance
         self.print_transactions = print_transactions
+    
 
         
     def check_balance(self): # returns the account ballance
         return f' your balance is {self.balance}'
 
     def deposit(self, amount):
-        return self.balance + amount
+        self.balance = self.balance + amount
+        return f'You deposited {amount} dollars. your new amount is {self.balance}'
+
+    def check_withdrawal(self, amount): # check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the
+        if amount < self.balance:
+            return True
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return f'You withdrew {amount} dollars. your new amount is {self.balance}'
    
-    # deposit(amount) deposits the given amount in the accoun
+   
+# check_withdrawal(amount) returns true if the withdrawn amount won't put the account in the negative
+# withdraw(amount) withdraws the amount from the account and returns it
+# calc_interest() returns the amount of interest calculated on the account
 
-my_atm = ATM()
 
-print(my_atm.check_balance())
-print(my_atm.deposit(4582))
-print(my_atm.check_balance())
+
+my_atm1 = ATM(3000,[])
+
+print(my_atm1.check_balance())
+print(my_atm1.deposit(4582))
+print(my_atm1.check_balance())
+print(my_atm1.check_withdrawal(20))
+print(my_atm1.withdraw(100))
+print(my_atm1.withdraw(100))
+print(my_atm1.withdraw(100))
